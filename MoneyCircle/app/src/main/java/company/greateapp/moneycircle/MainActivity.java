e456fa3f0327490f435ef47db0780a28aa0b6232
Gyanendra Singh Sirohi <gyanendra.s.sirohi@gmail.com>
2015-06-24 22:16:29 +0530

    local_gyan  to sprint

--- MoneyCircle/app/src/main/java/company/greateapp/moneycircle/MainActivity.java
+++ MoneyCircle/app/src/main/java/company/greateapp/moneycircle/MainActivity.java
@@ -2,6 +2,7 @@ package company.greateapp.moneycircle;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,7 +13,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Test","Prateek");
+        Log.d("Test", "Prateek");
+        Log.d("Test", "Gyan");
     }
 
     @Override
